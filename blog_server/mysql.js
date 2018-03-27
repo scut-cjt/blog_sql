0a009181c386d26645ebf4ce3662b56011975369
jtchen <jtchen@ewaytec.cn>
2018-03-27 14:48:33 +0800

    add

--- blog_server/mysql.js
+++ blog_server/mysql.js
@@ -34,12 +34,11 @@ db.connect(err => {
  * jtchen 2018/3/27
  */
 let query_sql = 'select * from test';
-db.query(query_sql, function(error,result){
-    if(error)
-        console.log(error.message);
+db.query(query_sql, (err, res) => {
+    if (err) console.log(err.message);
     else{
         console.log("================查询结果================");
-        console.log(result);
+        console.log(res);
     }
 })
 
@@ -49,24 +48,22 @@ db.query(query_sql, function(error,result){
  */
 let insert_sql = 'insert into test(id,name,age,password) values(?,?,?,?)';
 let insert_param = [101,'test_user_01',18,'123'];
-db.query(insert_sql, insert_param, function(error, result){
-    if(error)
-        console.log(error.message);
+db.query(insert_sql, insert_param, (err, res) => {
+    if (err) console.log(err.message);
     else{
         console.log("================插入结果================");
-        console.log('[db insert success! ]insert id: '+result.insertId);
+        console.log('[db insert success! ]insert id: '+res.insertId);
     }
 });
 
 /**
  * 修改
- * jtchen 2018/3/27
+ * jtchen 2018/3/27 
  */
 let update_sql = "update test set name = ?, password = ?, age = ? where id =?";
 let update_param = ['lxy2', '123456', 15, 2];
 db.query(update_sql, update_param, (err,res) => {
-    if(err)
-        console.log(err.message);
+    if (err) console.log(err.message);
     else{
         console.log("================修改结果================");
         console.log(res.affectedRows);
@@ -79,24 +76,18 @@ db.query(update_sql, update_param, (err,res) => {
  */
 let delet_sql = "delete from test where id = 102";
 db.query(delet_sql, (err,res) => {
-    if(err)
-        console.log(err.message);
+    if (err) console.log(err.message);
     else{
         console.log("================删除结果================");
         console.log(res.affectedRows);
     }
 })
 
-
-
 /**
  * 关闭数据库
  * jtchen 2018/3/27
  */
 db.end(err => {
-    if(err){
-        console.log('[query] - '+err);
-        return
-    }
+    if (err) console.log(err.message);
     console.log('[db end] success')
 })
